a42778170530245f61b82df5252e89028e8c0b03
Jakub <gawlikjakub9@gmail.com>
2023-01-05 18:20:10 +0100

    route guards and conditional showing buttons based on auth state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 //components
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+//authcontext
+import useAuthContext from "./hooks/useAuthContext";
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAuthContext from "../hooks/useAuthContext";
 
 const Navbar = () => {
+  const { user } = useAuthContext();
+
   return (
     <nav className="w-full h-20 bg-green-700 flex justify-around sm:justify-between items-center text-white">
       <h1 className="ml-4 mr-2 sm:ml-16 text-4xl font-mono">Centuś</h1>
       <div className="flex w-3/4 sm:w-1/5 justify-between mr-12 font-mono items-center">
-        <button className="bg-white text-green-700 px-4 py-2 rounded">
-          Logout
-        </button>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
+        {user && (
+          <button className="bg-white text-green-700 px-4 py-2 rounded">
+            Logout
+          </button>
+        )}
+        {!user && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign up</Link>
+          </>
+        )}
       </div>
     </nav>
   );
